f38f3bf594ab6f8875393315a5b4a71de1f20cbf
yannknnay <kevenyann@gmail.com>
2024-08-16 15:59:20 -0300

    Limpando o código

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const inputText = document.getElementById("input_text");
-const outputText = document.getElementById("output_text");
-const encryptButton = document.getElementById("crypt_btn"); // Corrigido de 'encrypt_btn' para 'crypt_btn'
-const decryptButton = document.getElementById("decrypt_btn");
-const copyButton = document.getElementById("copy_btn");
+const textoEntrada = document.getElementById("texto_entrada");
+const textoSaida = document.getElementById("texto_saida");
+const botaoCriptografar = document.getElementById("botao_criptografar");
+const botaoDescriptografar = document.getElementById("botao_descriptografar");
+const botaoCopiar = document.getElementById("botao_copiar");
 
 function criptografar(texto) {
     return texto
@@ -22,18 +22,33 @@ function descriptografar(texto) {
         .replace(/ufat/g, "u");
 }
 
-encryptButton.addEventListener("click", () => {
-    const textoCriptografado = criptografar(inputText.value);
-    outputText.value = textoCriptografado;
+function conferirLowerCase(texto) {
+    if (texto === texto.toLowerCase() && !/[áéíóúãõâêîôûç]/.test(texto)) {
+        return true;
+    } else {
+        textoSaida.value = "Digite apenas letras minúsculas e sem acento!"
+        return false;
+    }
+}
+
+botaoCriptografar.addEventListener("click", () => {
+    const texto = textoEntrada.value;
+    if (conferirLowerCase(texto)) {
+        const textoCriptografado = criptografar(texto);
+        textoSaida.value = textoCriptografado;
+    }
 });
 
-decryptButton.addEventListener("click", () => {
-    const textoDescriptografado = descriptografar(inputText.value);
-    outputText.value = textoDescriptografado;
+botaoDescriptografar.addEventListener("click", () => {
+    const texto = textoEntrada.value;
+    if (conferirLowerCase(texto)) {
+        const textoDescriptografado = descriptografar(texto);
+        textoSaida.value = textoDescriptografado;
+    }
 });
 
-copyButton.addEventListener("click", () => {
-    outputText.select();
+botaoCopiar.addEventListener("click", () => {
+    textoSaida.select();
     document.execCommand("copy");
-    outputText.value = "";
+    textoSaida.value = "";
 });
